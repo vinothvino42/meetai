ab8e60bc8b39aaf6e0391fdfd9ff9597a8f36f6e
vinothvino42 <vinothvino42@gmail.com>
2025-06-19 00:14:40 +0530

    feat(auth): handle page redirect based on auth session

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,6 +1,16 @@
 import { SignUpView } from "@/app/modules/auth/ui/views/sign-up-view";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const Page = () => {
+const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!!session) {
+    redirect("/");
+  }
   return <SignUpView />;
 };
 
