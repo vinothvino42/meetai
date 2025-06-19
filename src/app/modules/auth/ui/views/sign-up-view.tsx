ecef27a077fd5a91c05f68f980861bb139410161
vinothvino42 <vinothvino42@gmail.com>
2025-06-19 07:09:04 +0530

    refactor(auth): change logo background gradient color

--- src/app/modules/auth/ui/views/sign-up-view.tsx
+++ src/app/modules/auth/ui/views/sign-up-view.tsx
@@ -233,7 +233,7 @@ export const SignUpView = () => {
             </form>
           </Form>
 
-          <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+          <div className="bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center">
             <Image src="/logo.svg" alt="logo" width={92} height={92} />
             <p className="text-2xl font-semibold text-white">Meet.AI</p>
           </div>
