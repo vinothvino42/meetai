ea093fe1a76691961d38564fd616d40623c4ee5e
vinothvino42 <vinothvino42@gmail.com>
2025-06-19 19:40:19 +0530

    feat(dashboard): add dashboard command component

---
+++ src/app/modules/dashboard/ui/dashboard-command.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { CommandDialog, CommandInput } from "@/components/ui/command";
+import { CommandItem, CommandList } from "cmdk";
+import { Dispatch, SetStateAction } from "react";
+
+interface Props {
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export const DashboardCommand = ({ open, setOpen }: Props) => {
+  return (
+    <CommandDialog open={open} onOpenChange={setOpen}>
+      <CommandInput placeholder="Find a meeting or agent" />
+      <CommandList>
+        <CommandItem>Test</CommandItem>
+      </CommandList>
+    </CommandDialog>
+  );
+};
