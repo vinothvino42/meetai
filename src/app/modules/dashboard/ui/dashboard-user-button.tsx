439524bbd1008de10e28817852b217a4f0a0b452
vinothvino42 <vinothvino42@gmail.com>
2025-06-19 19:38:26 +0530

    refactor: add drawer for showing user account menus in mobile viewport

--- src/app/modules/dashboard/ui/dashboard-user-button.tsx
+++ src/app/modules/dashboard/ui/dashboard-user-button.tsx
@@ -7,13 +7,25 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { GeneratedAvatar } from "@/components/generated-avatar";
-import { ChevronDown, CreditCard, LogOut } from "lucide-react";
+import { ChevronDown, CreditCardIcon, LogOutIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { Button } from "@/components/ui/button";
 
 const DashboardUserButton = () => {
   const router = useRouter();
+  const isMobile = useIsMobile();
   const { data, isPending } = authClient.useSession();
 
   const onLogout = () => {
@@ -28,6 +40,51 @@ const DashboardUserButton = () => {
 
   if (isPending || !data?.user) {
     return null;
+  }
+
+  if (isMobile) {
+    return (
+      <Drawer>
+        <DrawerTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
+          {data.user.image ? (
+            <Avatar>
+              <AvatarImage src={data.user.image} />
+            </Avatar>
+          ) : (
+            <GeneratedAvatar
+              seed={data.user.name}
+              variant="initials"
+              className="size-9 mr-3"
+            />
+          )}
+          <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+            <p className="text-sm truncate w-full">{data.user.name}</p>
+            <p className="text-sm truncate w-full">{data.user.email}</p>
+          </div>
+          <ChevronDown className="size-4 shrink-0" />
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle className="font-medium truncate text-left">
+              {data.user.name}
+            </DrawerTitle>
+            <DrawerTitle className="text-sm font-normal text-muted-foreground truncate text-left">
+              {data.user.email}
+            </DrawerTitle>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button variant="outline" onClick={onLogout}>
+              <CreditCardIcon className="size-4 text-black" />
+              Billing
+            </Button>
+            <Button variant="outline" onClick={onLogout}>
+              <LogOutIcon className="size-4 text-black" />
+              Logout
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    );
   }
   return (
     <DropdownMenu>
@@ -61,14 +118,14 @@ const DashboardUserButton = () => {
         <DropdownMenuSeparator />
         <DropdownMenuItem className="cursor-pointer flex items-center justify-between">
           Billing
-          <CreditCard className="size-4" />
+          <CreditCardIcon className="size-4" />
         </DropdownMenuItem>
         <DropdownMenuItem
           className="cursor-pointer flex items-center justify-between"
           onClick={onLogout}
         >
           Logout
-          <LogOut className="size-4" />
+          <LogOutIcon className="size-4" />
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
