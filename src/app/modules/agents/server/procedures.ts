731630ab437257021f2f059b8054663c3317e015
vinothvino42 <vinothvino42@gmail.com>
2025-08-20 23:28:14 +0530

    feat(trpc): add protected procedure and agent insert schema

--- src/app/modules/agents/server/procedures.ts
+++ src/app/modules/agents/server/procedures.ts
@@ -1,10 +1,29 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+import {
+  createTRPCRouter,
+  baseProcedure,
+  protectedProcedure,
+} from "@/trpc/init";
+import { agentsInsertSchema } from "../schemas";
 
 export const agentsRouter = createTRPCRouter({
+  // TODO: Change `getMany` to use protectedProcedure
   getMany: baseProcedure.query(async () => {
     const data = await db.select().from(agents);
     return data;
   }),
+  create: protectedProcedure
+    .input(agentsInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [createdAgent] = await db
+        .insert(agents)
+        .values({
+          ...input,
+          userId: ctx.auth.user.id,
+        })
+        .returning();
+
+      return createdAgent;
+    }),
 });
