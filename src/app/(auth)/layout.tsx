ad44cf5acb617a2f6b53fc3163d4611e69171584
vinothvino42 <vinothvino42@gmail.com>
2025-08-20 06:23:44 +0530

    feat: setup trpc routes, query client, client and server

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className=" flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+    <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">{children}</div>
     </div>
   );
